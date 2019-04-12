0722af2f20a3beeffb4526d69e94203fdf51716f
richard-ohg <ricardoomar.angel@gmail.com>
2019-04-12 11:24:21 -0500

    borrado

--- clases/ejemplo4.asm
+++
@@ -1,8 +0,0 @@
-section .text
-global _start
-_start:
-    push 0xf00d
-    push 0xbaad
-    mov eax, 0xf00d
-    pop eax
-    
--- clases/overflow3.c
+++
@@ -1,9 +0,0 @@
-#include<string.h>
-void func(char* cadena){
-	char buffer[128];
-	strcpy(buffer,cadena);
-	puts(buffer);
-}
-void main(int argc, char* argv[]){
-	func(argv[1]);
-}
